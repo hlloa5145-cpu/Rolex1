4e5a8ea5dbd1e7c905971d45a88c7d986add7aa0
hlloa5145-cpu <hlloa5145@gmail.com>
2026-04-02 14:49:49 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0d0221",
+        card: "#1a0b3c",
+        accent: "#8b5cf6"
+      }
+    }
+  },
+  plugins: [],
+}
